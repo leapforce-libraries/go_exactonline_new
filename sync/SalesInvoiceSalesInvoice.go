f6f9025597b91e9b857fa926a0291bad65a3ffb6
caselongo <caselongo@gmail.com>
2021-06-04 20:46:16 +0200

    x

--- sync/SalesInvoiceSalesInvoice.go
+++ sync/SalesInvoiceSalesInvoice.go
@@ -18,15 +18,20 @@ type SalesInvoiceSalesInvoice struct {
 	AmountDiscountExclVat                float64           `json:"AmountDiscountExclVat"`
 	AmountFC                             float64           `json:"AmountFC"`
 	AmountFCExclVat                      float64           `json:"AmountFCExclVat"`
+	CostCenter                           types.GUID        `json:"CostCenter"`
+	CostCenterDescription                string            `json:"CostCenterDescription"`
+	CostUnit                             string            `json:"CostUnit"`
+	CostUnitDescription                  string            `json:"CostUnitDescription"`
 	Created                              *types.Date       `json:"Created"`
 	Creator                              types.GUID        `json:"Creator"`
 	CreatorFullName                      string            `json:"CreatorFullName"`
 	Currency                             string            `json:"Currency"`
+	CustomerItemCode                     string            `json:"CustomerItemCode"`
 	DeliverTo                            types.GUID        `json:"DeliverTo"`
 	DeliverToAddress                     types.GUID        `json:"DeliverToAddress"`
 	DeliverToContactPerson               types.GUID        `json:"DeliverToContactPerson"`
 	DeliverToContactPersonFullName       string            `json:"DeliverToContactPersonFullName"`
-	DeliverToName                        string            `json:"DeliverToName"`
+	DeliveryDate                         string            `json:"DeliveryDate"`
 	Description                          string            `json:"Description"`
 	Discount                             float64           `json:"Discount"`
 	DiscountType                         int16             `json:"DiscountType"`
@@ -35,8 +40,14 @@ type SalesInvoiceSalesInvoice struct {
 	DocumentNumber                       int32             `json:"DocumentNumber"`
 	DocumentSubject                      string            `json:"DocumentSubject"`
 	DueDate                              *types.Date       `json:"DueDate"`
+	Employee                             types.GUID        `json:"Employee"`
+	EmployeeFullName                     string            `json:"EmployeeFullName"`
+	EndTime                              *types.Date       `json:"EndTime"`
 	ExtraDutyAmountFC                    float64           `json:"ExtraDutyAmountFC"`
+	ExtraDutyPercentage                  float64           `json:"ExtraDutyPercentage"`
 	GAccountAmountFC                     float64           `json:"GAccountAmountFC"`
+	GLAccount                            types.GUID        `json:"GLAccount"`
+	GLAccountDescription                 string            `json:"GLAccountDescription"`
 	InvoiceDate                          *types.Date       `json:"InvoiceDate"`
 	InvoiceID                            types.GUID        `json:"InvoiceID"`
 	InvoiceNumber                        int32             `json:"InvoiceNumber"`
@@ -45,11 +56,16 @@ type SalesInvoiceSalesInvoice struct {
 	InvoiceToContactPersonFullName       string            `json:"InvoiceToContactPersonFullName"`
 	InvoiceToName                        string            `json:"InvoiceToName"`
 	IsExtraDuty                          bool              `json:"IsExtraDuty"`
+	Item                                 types.GUID        `json:"Item"`
+	ItemCode                             string            `json:"ItemCode"`
+	ItemDescription                      string            `json:"ItemDescription"`
 	Journal                              string            `json:"Journal"`
 	JournalDescription                   string            `json:"JournalDescription"`
+	LineNumber                           int32             `json:"LineNumber"`
 	Modified                             *types.Date       `json:"Modified"`
 	Modifier                             types.GUID        `json:"Modifier"`
 	ModifierFullName                     string            `json:"ModifierFullName"`
+	NetPrice                             float64           `json:"NetPrice"`
 	OrderDate                            *types.Date       `json:"OrderDate"`
 	OrderedBy                            types.GUID        `json:"OrderedBy"`
 	OrderedByContactPerson               types.GUID        `json:"OrderedByContactPerson"`
@@ -59,23 +75,39 @@ type SalesInvoiceSalesInvoice struct {
 	PaymentCondition                     string            `json:"PaymentCondition"`
 	PaymentConditionDescription          string            `json:"PaymentConditionDescription"`
 	PaymentReference                     string            `json:"PaymentReference"`
+	Pricelist                            types.GUID        `json:"Pricelist"`
+	PricelistDescription                 string            `json:"PricelistDescription"`
+	Project                              types.GUID        `json:"Project"`
+	ProjectDescription                   string            `json:"ProjectDescription"`
+	ProjectWBS                           types.GUID        `json:"ProjectWBS"`
+	ProjectWBSDescription                string            `json:"ProjectWBSDescription"`
+	Quantity                             float64           `json:"Quantity"`
 	Remarks                              string            `json:"Remarks"`
+	SalesOrder                           types.GUID        `json:"SalesOrder"`
+	SalesOrderLine                       types.GUID        `json:"SalesOrderLine"`
+	SalesOrderLineNumber                 int32             `json:"SalesOrderLineNumber"`
+	SalesOrderNumber                     int32             `json:"SalesOrderNumber"`
 	SalesPerson                          types.GUID        `json:"Salesperson"`
 	SalesPersonFullName                  string            `json:"SalespersonFullName"`
-	SelectionCode                        types.GUID        `json:"SelectionCode"`
-	SelectionCodeCode                    string            `json:"SelectionCodeCode"`
-	SelectionCodeDescription             string            `json:"SelectionCodeDescription"`
 	StarterSalesInvoiceStatus            int16             `json:"StarterSalesInvoiceStatus"`
 	StarterSalesInvoiceStatusDescription string            `json:"StarterSalesInvoiceStatusDescription"`
 	Status                               int16             `json:"Status"`
 	StatusDescription                    string            `json:"StatusDescription"`
+	Subscription                         types.GUID        `json:"Subscription"`
+	SubscriptionDescription              string            `json:"SubscriptionDescription"`
 	TaxSchedule                          types.GUID        `json:"TaxSchedule"`
 	TaxScheduleCode                      string            `json:"TaxScheduleCode"`
 	TaxScheduleDescription               string            `json:"TaxScheduleDescription"`
 	Type                                 int32             `json:"Type"`
 	TypeDescription                      string            `json:"TypeDescription"`
+	UnitCode                             string            `json:"UnitCode"`
+	UnitDescription                      string            `json:"UnitDescription"`
+	UnitPrice                            float64           `json:"UnitPrice"`
 	VATAmountDC                          float64           `json:"VATAmountDC"`
 	VATAmountFC                          float64           `json:"VATAmountFC"`
+	VATCode                              string            `json:"VATCode"`
+	VATCodeDescription                   string            `json:"VATCodeDescription"`
+	VATPercentage                        float64           `json:"VATPercentage"`
 	Warehouse                            types.GUID        `json:"Warehouse"`
 	WithholdingTaxAmountFC               float64           `json:"WithholdingTaxAmountFC"`
 	WithholdingTaxBaseAmount             float64           `json:"WithholdingTaxBaseAmount"`
