063076cbd4ab24ae23d353247ed28a4de6e9d91a
caselongo <caselongo@gmail.com>
2023-04-18 17:43:06 +0200

    added AccountClassification, AccountClassificationName

---
+++ crm/AccountClassification.go
@@ -0,0 +1,117 @@
+package exactonline
+
+import (
+	"fmt"
+	types "github.com/leapforce-libraries/go_types"
+	"strings"
+	"time"
+
+	errortools "github.com/leapforce-libraries/go_errortools"
+	utilities "github.com/leapforce-libraries/go_utilities"
+)
+
+// AccountClassification stores AccountClassification from exactonline
+type AccountClassification struct {
+	ID                                   types.Guid  `json:"ID"`
+	AccountClassificationName            types.Guid  `json:"AccountClassificationName"`
+	AccountClassificationNameDescription string      `json:"AccountClassificationNameDescription"`
+	Code                                 string      `json:"Code"`
+	Created                              *types.Date `json:"Created"`
+	Creator                              types.Guid  `json:"Creator"`
+	CreatorFullName                      string      `json:"CreatorFullName"`
+	Description                          string      `json:"Description"`
+	Division                             int32       `json:"Division"`
+	Modified                             *types.Date `json:"Modified"`
+	Modifier                             types.Guid  `json:"Modifier"`
+	ModifierFullName                     string      `json:"ModifierFullName"`
+}
+
+type GetAccountClassificationsCall struct {
+	urlNext string
+	service *Service
+}
+
+type GetAccountClassificationsCallParams struct {
+	ChamberOfCommerce *string
+	ModifiedAfter     *time.Time
+}
+
+func (service *Service) NewGetAccountClassificationsCall(params *GetAccountClassificationsCallParams) *GetAccountClassificationsCall {
+	call := GetAccountClassificationsCall{}
+	call.service = service
+
+	selectFields := utilities.GetTaggedTagNames("json", AccountClassification{})
+	call.urlNext = service.url(fmt.Sprintf("AccountClassifications?$select=%s", selectFields))
+	filter := []string{}
+
+	if params != nil {
+		if params.ChamberOfCommerce != nil {
+			filter = append(filter, fmt.Sprintf("ChamberOfCommerce eq '%s'", *params.ChamberOfCommerce))
+		}
+		if params.ModifiedAfter != nil {
+			filter = append(filter, service.DateFilter("Modified", "gt", params.ModifiedAfter, false, ""))
+		}
+	}
+
+	if len(filter) > 0 {
+		call.urlNext = fmt.Sprintf("%s&$filter=%s", call.urlNext, strings.Join(filter, " and "))
+	}
+
+	return &call
+}
+
+func (call *GetAccountClassificationsCall) Do() (*[]AccountClassification, *errortools.Error) {
+	if call.urlNext == "" {
+		return nil, nil
+	}
+
+	accountClassifications := []AccountClassification{}
+
+	next, err := call.service.Get(call.urlNext, &accountClassifications)
+	if err != nil {
+		return nil, err
+	}
+
+	call.urlNext = next
+
+	return &accountClassifications, nil
+}
+
+func (call *GetAccountClassificationsCall) DoAll() (*[]AccountClassification, *errortools.Error) {
+	accountClassifications := []AccountClassification{}
+
+	for true {
+		_accountClassifications, e := call.Do()
+		if e != nil {
+			return nil, e
+		}
+
+		if _accountClassifications == nil {
+			break
+		}
+
+		if len(*_accountClassifications) == 0 {
+			break
+		}
+
+		accountClassifications = append(accountClassifications, *_accountClassifications...)
+	}
+
+	return &accountClassifications, nil
+}
+
+func (service *Service) GetAccountClassification(id types.Guid) (*AccountClassification, *errortools.Error) {
+	url := service.url(fmt.Sprintf("AccountClassifications(guid'%s')", id.String()))
+
+	accountClassificationNew := AccountClassification{}
+
+	e := service.GetSingle(url, &accountClassificationNew)
+	if e != nil {
+		return nil, e
+	}
+	return &accountClassificationNew, nil
+}
+
+func (service *Service) GetAccountClassificationsCount(createdBefore *time.Time) (int64, *errortools.Error) {
+	return service.GetCount("AccountClassifications", createdBefore)
+}
---
+++ crm/AccountClassificationName.go
@@ -0,0 +1,115 @@
+package exactonline
+
+import (
+	"fmt"
+	types "github.com/leapforce-libraries/go_types"
+	"strings"
+	"time"
+
+	errortools "github.com/leapforce-libraries/go_errortools"
+	utilities "github.com/leapforce-libraries/go_utilities"
+)
+
+// AccountClassificationName stores AccountClassificationName from exactonline
+type AccountClassificationName struct {
+	ID               types.Guid  `json:"ID"`
+	Created          *types.Date `json:"Created"`
+	Creator          types.Guid  `json:"Creator"`
+	CreatorFullName  string      `json:"CreatorFullName"`
+	Description      string      `json:"Description"`
+	Division         int32       `json:"Division"`
+	Modified         *types.Date `json:"Modified"`
+	Modifier         types.Guid  `json:"Modifier"`
+	ModifierFullName string      `json:"ModifierFullName"`
+	SequenceNumber   int32       `json:"SequenceNumber"`
+}
+
+type GetAccountClassificationNamesCall struct {
+	urlNext string
+	service *Service
+}
+
+type GetAccountClassificationNamesCallParams struct {
+	ChamberOfCommerce *string
+	ModifiedAfter     *time.Time
+}
+
+func (service *Service) NewGetAccountClassificationNamesCall(params *GetAccountClassificationNamesCallParams) *GetAccountClassificationNamesCall {
+	call := GetAccountClassificationNamesCall{}
+	call.service = service
+
+	selectFields := utilities.GetTaggedTagNames("json", AccountClassificationName{})
+	call.urlNext = service.url(fmt.Sprintf("AccountClassificationNames?$select=%s", selectFields))
+	filter := []string{}
+
+	if params != nil {
+		if params.ChamberOfCommerce != nil {
+			filter = append(filter, fmt.Sprintf("ChamberOfCommerce eq '%s'", *params.ChamberOfCommerce))
+		}
+		if params.ModifiedAfter != nil {
+			filter = append(filter, service.DateFilter("Modified", "gt", params.ModifiedAfter, false, ""))
+		}
+	}
+
+	if len(filter) > 0 {
+		call.urlNext = fmt.Sprintf("%s&$filter=%s", call.urlNext, strings.Join(filter, " and "))
+	}
+
+	return &call
+}
+
+func (call *GetAccountClassificationNamesCall) Do() (*[]AccountClassificationName, *errortools.Error) {
+	if call.urlNext == "" {
+		return nil, nil
+	}
+
+	accountClassificationNames := []AccountClassificationName{}
+
+	next, err := call.service.Get(call.urlNext, &accountClassificationNames)
+	if err != nil {
+		return nil, err
+	}
+
+	call.urlNext = next
+
+	return &accountClassificationNames, nil
+}
+
+func (call *GetAccountClassificationNamesCall) DoAll() (*[]AccountClassificationName, *errortools.Error) {
+	accountClassificationNames := []AccountClassificationName{}
+
+	for true {
+		_accountClassificationNames, e := call.Do()
+		if e != nil {
+			return nil, e
+		}
+
+		if _accountClassificationNames == nil {
+			break
+		}
+
+		if len(*_accountClassificationNames) == 0 {
+			break
+		}
+
+		accountClassificationNames = append(accountClassificationNames, *_accountClassificationNames...)
+	}
+
+	return &accountClassificationNames, nil
+}
+
+func (service *Service) GetAccountClassificationName(id types.Guid) (*AccountClassificationName, *errortools.Error) {
+	url := service.url(fmt.Sprintf("AccountClassificationNames(guid'%s')", id.String()))
+
+	accountClassificationNameNew := AccountClassificationName{}
+
+	e := service.GetSingle(url, &accountClassificationNameNew)
+	if e != nil {
+		return nil, e
+	}
+	return &accountClassificationNameNew, nil
+}
+
+func (service *Service) GetAccountClassificationNamesCount(createdBefore *time.Time) (int64, *errortools.Error) {
+	return service.GetCount("AccountClassificationNames", createdBefore)
+}
